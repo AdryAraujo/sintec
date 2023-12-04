f0d6333ca56df0739e10b9f42ad5a4dd1e24a93d
Adryelle Silva <191036532940@academico.ufs.br>
2023-12-04 10:30:48 -0300

    corrigindo login

--- type/sintec-type/src/pages/Login.tsx
+++ type/sintec-type/src/pages/Login.tsx
@@ -3,17 +3,13 @@ import React from 'react';
 import LogoLogin from "../assets/logoLogin.svg"
 import Image from "next/image";
 import Logo from "../assets/logo_sintec.svg"
-import { Button, ButtonGroup, Input, InputGroup, InputLeftElement, Link, InputRightElement, Stack, Icon } from "@chakra-ui/react";
-import { HiOutlineMail, HiMiniMoon, HiEyeOff, HiEye } from "react-icons/hi";
+import { Button, ButtonGroup, Input, InputGroup, InputLeftElement, Link, InputRightElement, Stack, Icon, Flex } from "@chakra-ui/react";
+import { HiOutlineMail, HiEyeOff, HiEye } from "react-icons/hi";
 import { IoKeyOutline, IoMoon } from "react-icons/io5";
 import { FiLogIn } from "react-icons/fi";
 import { Router } from "next/router";
 import { useRouter } from 'next/router';
 import { useState } from "react";
-
-
-
-
 
 export default function Login() {
     const router = useRouter();
@@ -24,67 +20,70 @@ export default function Login() {
     };
 
     return (
-        <div className={styles.container}>
-            <div className={styles.logo}>
-                <Image alt="" src={LogoLogin} />
-            </div>
-            {/* <IoMoon className={styles.icon}/> */}
-            <div className={styles.form_login}>
-                <Image className={styles.logoSintec1Icon} alt="" src={Logo} />
-                <div className={styles.containerInputs}>
-                    <InputGroup >
-                        <InputLeftElement pointerEvents='none'>
-                            <HiOutlineMail />
-                        </InputLeftElement>
-                        <Input
-                            variant='filled'
-                            placeholder='Filled'
-                            type='tel'
-                            placeholder='Digite seu email aqui!'
-                            backgroundColor='#ffffff'
-                            borderColor='gray.200'
-                            width='450px'
-                            marginBottom='20px' />
-                    </InputGroup>
-                    <InputGroup size='md'>
-                        <InputLeftElement pointerEvents='none'>
-                            <IoKeyOutline />
-                        </InputLeftElement>
-                        <Input
-                            variant='filled'
-                            placeholder='Filled'
-                            type='password'
-                            placeholder='Digite sua senha aqui!'
-                            backgroundColor='#ffffff'
-                            borderColor='gray.200'
-                            pr='4.5rem'
-                            type={show ? 'text' : 'password'}
-                            width='450px'
-                        />
-                        <InputRightElement width="2.8rem">
-                            <Button h="1.75rem" size="sm" onClick={handleClick}>
-                                <Icon as={show ? HiEyeOff : HiEye} />
-                            </Button>
-                        </InputRightElement>
-                    </InputGroup>
-                    <Link className={styles.esqueceuSuaSenha} href={''}
-                        color='#6750a4'
-                        marginBottom='20px'
-                    >Esqueceu sua senha?</Link>
-                    <Button
-                        leftIcon={<FiLogIn />}
-                        backgroundColor='cyan.300'
-                        variant='solid'
-                        color='white'
-                        width='212px'
-                        height='32px'
-                        onClick={handleLogin}
-                        alignSelf={'center'}
-                    >
-                        Login
-                    </Button>
-                </div>
-            </div>
-        </div>
+        <Flex className={styles.container}>
+            <Flex className={styles.containerLogo}>
+                <Flex className={styles.logo}>
+                    <Image alt="" src={LogoLogin} />
+                </Flex>
+                {/* <IoMoon className={styles.icon}/> */}
+                <Flex className={styles.form_login}>
+                    <Image src={Logo} />
+                    <Flex className={styles.containerInputs}>
+                        <InputGroup >
+                            <InputLeftElement pointerEvents='none'>
+                                <HiOutlineMail />
+                            </InputLeftElement>
+                            <Input
+                                variant='filled'
+                                placeholder='Filled'
+                                type='tel'
+                                placeholder='Digite seu email aqui!'
+                                backgroundColor='#ffffff'
+                                borderColor='gray.200'
+                                width='450px'
+                                marginBottom='20px' />
+                        </InputGroup>
+                        <InputGroup size='md'>
+                            <InputLeftElement pointerEvents='none'>
+                                <IoKeyOutline />
+                            </InputLeftElement>
+                            <Input
+                                variant='filled'
+                                placeholder='Filled'
+                                type='password'
+                                placeholder='Digite sua senha aqui!'
+                                backgroundColor='#ffffff'
+                                borderColor='gray.200'
+                                pr='4.5rem'
+                                type={show ? 'text' : 'password'}
+                                width='450px'
+                            />
+                            <InputRightElement width="2.8rem">
+                                <Button h="1.75rem" size="sm" onClick={handleClick}>
+                                    <Icon as={show ? HiEyeOff : HiEye} />
+                                </Button>
+                            </InputRightElement>
+                        </InputGroup>
+                        <Link className={styles.esqueceuSuaSenha} href={''}
+                            color='#6750a4'
+                            marginBottom='20px'
+                        >Esqueceu sua senha?</Link>
+                        <Button
+                            leftIcon={<FiLogIn />}
+                            backgroundColor='cyan.300'
+                            variant='solid'
+                            color='white'
+                            width='212px'
+                            height='32px'
+                            onClick={handleLogin}
+                            alignSelf={'center'}
+                        >
+                            Login
+                        </Button>
+                    </Flex>
+                </Flex>
+            </Flex>
+        </Flex>
+
     )
 }
--- type/sintec-type/src/pages/index.tsx
+++ type/sintec-type/src/pages/index.tsx
@@ -1,10 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '@/styles/Home.module.css'
-import Login from  './Login'
-import Cadastro from './CadastroUser'
-import Sidebar from '@/components/Navbar/Sidebar'
-import CircCad from './CircCad'
+import Login from './Login'
 
 export default function Home() {
   return (
@@ -15,7 +10,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Cadastro/>
+      <Login/>
     </>
   )
 }
