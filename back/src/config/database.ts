e4f5eb0c52884cb7e91c0d5ba5c8e8499b87763b
Adryelle Silva <191036532940@academico.ufs.br>
2023-12-12 11:08:43 -0300

    não sei mais o q fazer

--- back/src/config/database.ts
+++ back/src/config/database.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize-typescript";
 import * as dotenv from "dotenv";
+import { Usere } from "../model/Usere"
 dotenv.config();
-
 
 class Database {
   public sequelize: Sequelize | undefined;
@@ -10,8 +10,7 @@ class Database {
   private POSTGRES_HOST = process.env.POSTGRES_HOST as string;
   private POSTGRES_PORT = process.env.POSTGRES_PORT as unknown as number;
   private POSTGRES_USER = process.env.POSTGRES_USER as unknown as string;
-  private POSTGRES_PASSWORD = process.env
-    .POSTGRES_PASSWORD as unknown as string;
+  private POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD as unknown as string;
 
   constructor() {
     this.connectToPostgreSQL();
@@ -25,18 +24,23 @@ class Database {
       host: this.POSTGRES_HOST,
       port: this.POSTGRES_PORT,
       dialect: "postgres",
+      models: [Usere],
     });
 
     await this.sequelize
       .authenticate()
       .then(() => {
-        console.log(
-          "Conexão feita com sucesso."
-        );
+        console.log("Conexão feita com sucesso.");
+        this.createTables(); // Chama o método para criar as tabelas após a conexão bem-sucedida
       })
       .catch((err) => {
         console.error("Não foi possível se conectar com o banco:", err);
       });
+  }
+
+  private async createTables() {
+    await this.sequelize?.sync({ alter: true }); // Sincroniza os modelos com o banco de dados
+    console.log("Tabelas criadas com sucesso.");
   }
 }
 
