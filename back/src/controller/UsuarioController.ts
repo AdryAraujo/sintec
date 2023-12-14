fc0f1838cd78ff5d9d4e857975de56c65f85d642
Adryelle Silva <191036532940@academico.ufs.br>
2023-12-14 07:53:40 -0300

    finalizando usuarioController

--- back/src/controller/UsuarioController.ts
+++ back/src/controller/UsuarioController.ts
@@ -2,9 +2,9 @@ import { Request, Response } from "express"
 import { Usuario } from "../model/Usuario.model"
 import { UsuarioRepo } from "../repository/UsuarioRepo";
 
-class UsuarioController{
-    async create(req: Request, res: Response){
-        try{
+class UsuarioController {
+    async create(req: Request, res: Response) {
+        try {
 
             const new_usuario = new Usuario();
             new_usuario.login_rede = req.body.login_rede;
@@ -24,7 +24,7 @@ class UsuarioController{
                 status: "Criado com sucesso!",
                 message: "Criado com sucesso!"
             })
-        }catch(err){
+        } catch (err) {
             res.status(500).json({
                 status: "Internal Server Error!",
                 message: "Internal Server Error!"
@@ -32,8 +32,8 @@ class UsuarioController{
         }
     }
 
-    async delete(req: Request, res: Response){
-        try{
+    async delete(req: Request, res: Response) {
+        try {
 
             let cd_usuario = parseInt(req.params["cd_usuario"])
             await new UsuarioRepo().delete(cd_usuario)
@@ -42,7 +42,7 @@ class UsuarioController{
                 status: "Ok!",
                 message: "Deletado com sucesso!"
             })
-        }catch(err){
+        } catch (err) {
             res.status(500).json({
                 status: "Internal Server Error!",
                 message: "Internal Server Error!"
@@ -50,21 +50,72 @@ class UsuarioController{
         }
     }
 
-    async findAll(req: Request, res: Response){
-        try{
-
-            const new_usuario = await new UsuarioRepo().retrieveAll
+    async findAll(req: Request, res: Response) {
+        try {
+            const new_usuario = await new UsuarioRepo().retrieveAll()
 
             res.status(200).json({
                 status: "Ok!",
-                message: "Deletado com sucesso!",
-                data: new_usuario
+                message: "Busca realizada com sucesso!",
+                data: new_usuario,
             })
-        }catch(err){
+        } catch (err) {
             res.status(500).json({
                 status: "Internal Server Error!",
                 message: "Internal Server Error!"
             })
         }
     }
+
+    async findById(req: Request, res: Response) {
+        try {
+            let cd_usuario = parseInt(req.params["cd_usuario"])
+            const new_usuario = await new UsuarioRepo().retrieveById(cd_usuario)
+
+            res.status(200).json({
+                status: "Ok!",
+                message: "Busca realizada com sucesso!",
+                data: new_usuario,
+            })
+        } catch (err) {
+            res.status(500).json({
+                status: "Internal Server Error!",
+                message: "Internal Server Error!"
+            })
+        }
+    }
+
+    async update(req: Request, res: Response) {
+        try {
+            let cd_usuario = parseInt(req.params["cd_usuario"]);
+            const new_usuario = new Usuario();
+
+            new_usuario.cd_usuario = cd_usuario;
+            new_usuario.login_rede = req.body.login_rede;
+            new_usuario.nm_usuario = req.body.nm_usuario;
+            new_usuario.login_rede = req.body.login_rede;
+            new_usuario.senha_rede = req.body.senha_rede;
+            new_usuario.nm_usuario = req.body.nm_usuario;
+            new_usuario.fl_usuario_ativo = req.body.fl_usuario_ativo;
+            new_usuario.cd_user_inclusao_usuario = req.body.cd_user_inclusao_usuario;
+            new_usuario.dt_alteracao_usuario = req.body.dt_alteracao_usuario;
+            new_usuario.dt_inclusao_usuario = req.body.dt_inclusao_usuario;
+            new_usuario.cd_user_alteracao_usuario = req.body.cd_user_alteracao_usuario;
+
+            await new UsuarioRepo().update(new_usuario);
+
+            res.status(200).json({
+                status: "Ok!",
+                message: "Successfully updated Usuario data!",
+            });
+        } catch (err) {
+            res.status(500).json({
+                status: "Internal Server Error!",
+                message: "Internal Server Error!",
+            });
+        }
+    }
+
 }
+
+export default new UsuarioController()
