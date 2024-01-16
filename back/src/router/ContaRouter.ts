5151819c555d9b7be014f97e122cdc4dcf323369
Adryelle Silva <191036532940@academico.ufs.br>
2024-01-16 10:58:25 -0300

    ajustes

--- back/src/router/ContaRouter.ts
+++ back/src/router/ContaRouter.ts
@@ -7,39 +7,39 @@ import { updateContaSchema } from "../schema/ContaSchema";
 import BaseRoutes from "./base/BaseRouter";
 
 class ContaRoutes extends BaseRoutes {
-    routes(): void {
+  routes(): void {
 
-        //Conta
-        this.router.post("/createConta", auth, ContaController.create);
-        this.router.get("/procurarConta", auth, ContaController.getAll);
-        this.router.patch(
-            "/alterarConta/:cd_conta_pk", auth, 
-            ContaController.update
-          );
-        this.router.delete("/delete/:cd_conta_pk", auth, ContaController.delete);
-        this.router.get("/procurarConta/:cd_conta_pk", auth, ContaController.findById);
+    //Conta
+    this.router.post("/createConta", auth, ContaController.create);
+    this.router.get("/procurarConta", auth, ContaController.getAll);
+    this.router.patch(
+      "/alterarConta/:cd_conta_pk", auth,
+      ContaController.update
+    );
+    this.router.delete("/delete/:cd_conta_pk", auth, ContaController.delete);
+    this.router.get("/procurarConta/:cd_conta_pk", auth, ContaController.findById);
 
-        // Contrato
-        this.router.post("/createContrato", auth, ContratoController.create);
-        this.router.get("/procurarContrato", auth, ContratoController.getAll);
-        this.router.patch(
-            "/alterarContrato/:cd_contrato_pk", auth,
-            ContratoController.update
-          );
-        this.router.delete("/delete/:cd_contrato_pk", auth, ContratoController.delete);
-        this.router.get("/procurarContrato/:cd_contrato_pk", auth, ContratoController.findById);
+    // Contrato
+    this.router.post("/createContrato", auth, ContratoController.create);
+    this.router.get("/procurarContrato", auth, ContratoController.getAll);
+    this.router.patch(
+      "/alterarContrato/:cd_contrato_pk", auth,
+      ContratoController.update
+    );
+    this.router.delete("/delete/:cd_contrato_pk", auth, ContratoController.delete);
+    this.router.get("/procurarContrato/:cd_contrato_pk", auth, ContratoController.findById);
 
-        //Circuito
-        this.router.post("/createCircuito", auth, CircuitoController.create);
-        this.router.get("/procurarCircuito", auth, CircuitoController.getAll);
-        this.router.patch(
-            "/alterarCircuito/:cd_circuito_pk", auth,
-            CircuitoController.update
-          );
-        this.router.delete("/delete/:cd_circuito_pk", auth, CircuitoController.delete);
-        this.router.get("/procurarCircuito/:cd_circuito_pk", auth, CircuitoController.findById);
+    //Circuito
+    this.router.post("/createCircuito", auth, CircuitoController.create);
+    this.router.get("/procurarCircuito", auth, CircuitoController.getAll);
+    this.router.patch(
+      "/alterarCircuito/:cd_circuito_pk", auth,
+      CircuitoController.update
+    );
+    this.router.delete("/delete/:cd_circuito_pk", auth, CircuitoController.delete);
+    this.router.get("/procurarCircuito/:cd_circuito_pk", auth, CircuitoController.findById);
 
-    }
+  }
 }
 
 export default new ContaRoutes().router;
--- back/src/router/UsuarioRouter.ts
+++ back/src/router/UsuarioRouter.ts
@@ -18,7 +18,7 @@ class UsuarioRoutes extends BaseRoutes {
     this.router.post("/register", UsuarioController.register);
     this.router.patch(
       "alterarUsuario/:cd_usuario",
-      validate(updateUsuarioSchema), 
+      validate(updateUsuarioSchema),
       UsuarioController.update
     );
     this.router.delete("/:cd_usuario", UsuarioController.delete);
@@ -28,69 +28,69 @@ class UsuarioRoutes extends BaseRoutes {
 
     //Fornecedor
     this.router.post("/createFornecedor", auth, FornecedorController.create);
-        this.router.get("/procurarFornecedor", auth, FornecedorController.getAll);
-        this.router.patch(
-            "/alterarFornecedor/:cd_fornecedor_pk",
-            auth, 
-            FornecedorController.update
-          );
-        this.router.delete("/delete/:cd_fornecedor_pk", auth, FornecedorController.delete);
-        this.router.get("/procurarFornecedor/:cd_fornecedor_pk", auth, FornecedorController.findById);
+    this.router.get("/procurarFornecedor", auth, FornecedorController.getAll);
+    this.router.patch(
+      "/alterarFornecedor/:cd_fornecedor_pk",
+      auth,
+      FornecedorController.update
+    );
+    this.router.delete("/delete/:cd_fornecedor_pk", auth, FornecedorController.delete);
+    this.router.get("/procurarFornecedor/:cd_fornecedor_pk", auth, FornecedorController.findById);
 
     //Municipio
     this.router.post("/createEstado", auth, MunicipioController.create);
-        this.router.get("/procurarMunicipio", auth, MunicipioController.getAll);
-        this.router.patch(
-            "/alterarMunicipio/:cd_municipio_pk",
-            auth, 
-            MunicipioController.update
-          );
-        this.router.delete("/delete/:cd_municipio_pk", auth, MunicipioController.delete);
-        this.router.get("/procurarMunicipio/:cd_municipio_pk", auth, MunicipioController.findById);
+    this.router.get("/procurarMunicipio", auth, MunicipioController.getAll);
+    this.router.patch(
+      "/alterarMunicipio/:cd_municipio_pk",
+      auth,
+      MunicipioController.update
+    );
+    this.router.delete("/delete/:cd_municipio_pk", auth, MunicipioController.delete);
+    this.router.get("/procurarMunicipio/:cd_municipio_pk", auth, MunicipioController.findById);
 
     //Estado
     this.router.post("/createEstado", auth, EstadoController.create);
-        this.router.get("/procurarEstado", auth, EstadoController.getAll);
-        this.router.patch(
-            "/alterarEstado/:cd_estado_pk",
-            auth, 
-            EstadoController.update
-          );
-        this.router.delete("/delete/:cd_estado_pk", auth, EstadoController.delete);
-        this.router.get("/procurarEstado/:cd_estado_pk", auth, EstadoController.findById);
+    this.router.get("/procurarEstado", auth, EstadoController.getAll);
+    this.router.patch(
+      "/alterarEstado/:cd_estado_pk",
+      auth,
+      EstadoController.update
+    );
+    this.router.delete("/delete/:cd_estado_pk", auth, EstadoController.delete);
+    this.router.get("/procurarEstado/:cd_estado_pk", auth, EstadoController.findById);
 
     //Pais
     this.router.post("/createPais", auth, PaisController.create);
-        this.router.get("/procurarPais", auth, PaisController.getAll);
-        this.router.patch(
-            "/alterarPais/:cd_pais_pk",
-            auth, 
-            PaisController.update
-          );
-        this.router.delete("/delete/:cd_pais_pk", auth, PaisController.delete);
-        this.router.get("/procurarPais/:cd_pais_pk", auth, PaisController.findById);
+    this.router.get("/procurarPais", auth, PaisController.getAll);
+    this.router.patch(
+      "/alterarPais/:cd_pais_pk",
+      auth,
+      PaisController.update
+    );
+    this.router.delete("/delete/:cd_pais_pk", auth, PaisController.delete);
+    this.router.get("/procurarPais/:cd_pais_pk", auth, PaisController.findById);
 
     //Unidade_adm
     this.router.post("/createUnidade_adm", auth, Unidade_admController.create);
     this.router.get("/procurarUnidade_adm", auth, Unidade_admController.getAll);
     this.router.patch(
-        "/alterarUnidade_adm/:cd_unidade_pk",
-        auth, 
-        Unidade_admController.update
-      );
+      "/alterarUnidade_adm/:cd_unidade_pk",
+      auth,
+      Unidade_admController.update
+    );
     this.router.delete("/delete/:cd_unidade_pk", auth, Unidade_admController.delete);
     this.router.get("/procurarUnidade_adm/:cd_unidade_pk", auth, Unidade_admController.findById);
 
     //Bairro
     this.router.post("/createBairroController", auth, BairroController.create);
-        this.router.get("/procurarc", auth, BairroController.getAll);
-        this.router.patch(
-            "/alterarBairro/:cd_bairro_pk",
-            auth, 
-            BairroController.update
-          );
-        this.router.delete("/delete/:cd_bairro_pk", auth, BairroController.delete);
-        this.router.get("/procurarBairro/:cd_bairro_pk", auth, BairroController.findById);
+    this.router.get("/procurarc", auth, BairroController.getAll);
+    this.router.patch(
+      "/alterarBairro/:cd_bairro_pk",
+      auth,
+      BairroController.update
+    );
+    this.router.delete("/delete/:cd_bairro_pk", auth, BairroController.delete);
+    this.router.get("/procurarBairro/:cd_bairro_pk", auth, BairroController.findById);
   }
 }
 
